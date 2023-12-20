158251ff7123dbfa44cddcf5aa2add26948d6fd0
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2023-12-20 14:43:43 -0300

    Customização do email de redefinição de senha

---
+++ app/Mail/meuResetDeSenha.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class meuResetDeSenha extends Notification
+{
+    use Queueable;
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Redefinir Senha')
+            ->greeting('Olá!')
+            ->line('Para redefinr a sua senha de acesso clique no botão abaixo.')
+            ->action('REDEFINIR SENHA', route('password.reset', $this->token))
+            ->line('Se você não quer mudar a sua senha basta desconsiderar este e-mail.')
+            ->salutation("Atenciosamente, " . env('APP_NAME') . '.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
