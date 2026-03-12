89816d5cca7075a6b66146c0880bb65561bfb389
Cristiano7988 <cristianooliveiramorales@gmail.com>
2026-03-12 10:36:12 -0300

    Adicionando a página de visualização do usuário

--- resources/js/pages/users/show.tsx
+++ resources/js/pages/users/show.tsx
@@ -16,11 +16,34 @@ export default function Show(props: any) {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Usuário" />
 
-            <div className="flex flex-wrap gap-4 p-4">
-                <div className='flex gap-4 p-4'>
-                    <div className="flex flex-col gap-2">
+            <div className='flex flex-col gap-4 p-4'>
+                <div className="flex gap-2">
+                    <p><strong>Nome:</strong>{user.nome}</p>
+                    <p><strong>Email</strong>{user.email}</p>
+                </div>
+                <div className="flex gap-2">
+                    <p><strong>CPF:</strong>{user.cpf}</p>
+                    <p><strong>CNPJ:</strong>{user.cnpj}</p>
+                </div>
+                <div className="flex gap-2">
+                    <p><strong>Whatsapp:</strong>{user.whatsapp}</p>
+                    <p><strong>Instagram:</strong>{user.instagram}</p>
+                </div>
 
-                    </div>
+                <p><strong>CEP:</strong>{user.cep}</p>
+
+                <div className="flex gap-2">
+                    <p><strong>País:</strong>{user.pais}</p>
+                    <p><strong>Estado:</strong>{user.estado}</p>
+                </div>
+                <div className="flex gap-2">
+                    <p><strong>Cidade:</strong>{user.cidade}</p>
+                    <p><strong>Bairro:</strong>{user.bairro}</p>
+                </div>
+                <div className="flex gap-2">
+                    <p><strong>Logradouro:</strong>{user.logradouro}</p>
+                    <p><strong>Número:</strong>{user.numero}</p>
+                    <p><strong>Complemento:</strong>{user.complemento}</p>
                 </div>
             </div>
         </AppLayout>
