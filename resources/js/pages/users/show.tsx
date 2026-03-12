fe8dc574d2a7ae569d4593c58f310bdff4100308
Cristiano7988 <cristianooliveiramorales@gmail.com>
2026-03-12 12:13:34 -0300

    Adição dos alunos relacionados ao usuário visualizado

--- resources/js/pages/users/show.tsx
+++ resources/js/pages/users/show.tsx
@@ -1,6 +1,6 @@
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -16,35 +16,48 @@ export default function Show(props: any) {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Usuário" />
 
-            <div className='flex flex-col gap-4 p-4'>
-                <div className="flex gap-2">
-                    <p><strong>Nome:</strong>{user.nome}</p>
-                    <p><strong>Email</strong>{user.email}</p>
+            <div className='flex flex-col gap-4 p-4 max-w-xl'>
+                <div className="flex gap-2 justify-between">
+                    <p><strong>Nome:</strong> {user.nome}</p>
+                    <p><strong>Email:</strong> {user.email}</p>
                 </div>
                 <div className="flex gap-2">
-                    <p><strong>CPF:</strong>{user.cpf}</p>
-                    <p><strong>CNPJ:</strong>{user.cnpj}</p>
+                    <p><strong>CPF:</strong> {user.cpf}</p>
+                    <p><strong>CNPJ:</strong> {user.cnpj}</p>
                 </div>
                 <div className="flex gap-2">
-                    <p><strong>Whatsapp:</strong>{user.whatsapp}</p>
-                    <p><strong>Instagram:</strong>{user.instagram}</p>
+                    <p><strong>Whatsapp:</strong> {user.whatsapp}</p>
+                    <p><strong>Instagram:</strong> {user.instagram}</p>
                 </div>
 
-                <p><strong>CEP:</strong>{user.cep}</p>
+                <p><strong>CEP:</strong> {user.cep}</p>
 
                 <div className="flex gap-2">
-                    <p><strong>País:</strong>{user.pais}</p>
-                    <p><strong>Estado:</strong>{user.estado}</p>
+                    <p><strong>País:</strong> {user.pais}</p>
+                    <p><strong>Estado:</strong> {user.estado}</p>
                 </div>
                 <div className="flex gap-2">
-                    <p><strong>Cidade:</strong>{user.cidade}</p>
-                    <p><strong>Bairro:</strong>{user.bairro}</p>
+                    <p><strong>Cidade:</strong> {user.cidade}</p>
+                    <p><strong>Bairro:</strong> {user.bairro}</p>
                 </div>
                 <div className="flex gap-2">
-                    <p><strong>Logradouro:</strong>{user.logradouro}</p>
-                    <p><strong>Número:</strong>{user.numero}</p>
-                    <p><strong>Complemento:</strong>{user.complemento}</p>
+                    <p><strong>Logradouro:</strong> {user.logradouro}</p>
+                    <p><strong>Número:</strong> {user.numero}</p>
+                    <p><strong>Complemento:</strong> {user.complemento}</p>
                 </div>
+
+                <hr />
+                
+                {!!user.alunos.length && (
+                    <>
+                        <h2 className='text-lg font-bold'>Alunos Vinculados</h2>
+                        <ul className='list-disc list-inside'>
+                            {user.alunos.map((aluno: any) => (
+                                <li key={aluno.id}><Link key={aluno.id} href={`/alunos/${aluno.id}`}>{aluno.nome}</Link></li>
+                            ))}
+                        </ul>
+                    </>
+                )}
             </div>
         </AppLayout>
     );
